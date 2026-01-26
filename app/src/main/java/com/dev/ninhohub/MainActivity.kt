202f257ade66e4753ca973c23fdca6795bdd21c9
Márcio Albuquerque <o.marcio.albu@gmail.com>
2026-01-26 07:39:29 -0300

    [CHORE][#9]: Atualizar libs

--- app/src/main/java/com/dev/ninhohub/MainActivity.kt
+++ app/src/main/java/com/dev/ninhohub/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.dev.ninhohub.core.navigation.AppNavHost
 import com.dev.ninhohub.core.ui.theme.NinhoHubTheme
+import com.dev.ninhohub.navigation.AppNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
